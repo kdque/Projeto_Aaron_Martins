66406248e3d11975138c4bee8c563335ef93f0f5
ana ornelas <76130077+kdque@users.noreply.github.com>
2024-10-28 19:45:44 -0300

    metodo getSoma

---
+++ A.php
@@ -0,0 +1,34 @@
+<?php
+class A {
+    private int $A1;
+    private float $A2;
+
+    public function getA1(): int {
+        return $this->A1;
+    }
+
+    public function setA1(int $A1): void {
+        $this->A1 = $A1;
+    }
+
+    public function getA2(): float {
+        return $this->A2;
+    }
+
+    public function setA2(float $A2): void {
+        $this->A2 = $A2;
+    }
+
+    public function MA1() {
+        echo "Método MA1()";
+    }
+
+    public function MA2() {
+        echo "Método MA2()";
+    }
+
+    public function getSoma(int $a, int $b): int {
+        return $a + $b;
+    }
+}
+?>
